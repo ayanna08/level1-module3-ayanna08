2671d04c30c070a541a4d8db6a4672e630df95c5
jaLeague <jackie.arseneau@jointheleague.org>
2020-10-20 01:16:35 +0000

    Initial commit

---
+++ src/_02_backpack/BackpackRunner.java
@@ -0,0 +1,7 @@
+package _02_backpack;
+
+public class BackpackRunner {
+	public static void main(String[] args) {
+		new Backpack().packAndCheck();
+	}
+}
---
+++ src/_03_jars/_2_jukebox/JukeboxRunner.java
@@ -0,0 +1,13 @@
+package _03_jars._2_jukebox;
+/*
+ *    Copyright (c) The League of Amazing Programmers 2013-2019
+ *    Level 1
+ */
+
+import javax.swing.SwingUtilities;
+
+public class JukeboxRunner {
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Jukebox());
+	}
+}
