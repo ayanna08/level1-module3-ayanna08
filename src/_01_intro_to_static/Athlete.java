dcc6653838661781213b9a48d2015fe24819282f
root <root@9699a64f5c40>
2020-10-27 01:29:06 +0000

    Day 13 pt 2

---
+++ src/_01_intro_to_static/Athlete.java
@@ -0,0 +1,16 @@
+package _01_intro_to_static;
+
+public class Athlete {
+    static int nextBibNumber;
+    static String raceLocation = "New York";
+    static String raceStartTime = "9.00am";
+
+    String name;
+    int speed;
+    int bibNumber;
+
+    Athlete (String name, int speed){
+        this.name = name;
+        this.speed = speed;
+    }
+}
---
+++ src/_01_intro_to_static/Intro_to_Static.java
@@ -0,0 +1,14 @@
+package _01_intro_to_static;
+
+public class Intro_to_Static {
+	
+    public static void main(String[] args) {
+        //create two athletes
+        //print their names, bibNumbers, and the location of their race.
+    	Athlete Mia = new Athlete("Mia", 14);
+    	
+    	Athlete Maria = new Athlete("Maria", 10);
+    	
+    	
+    }
+}
